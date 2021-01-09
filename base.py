d45896e66421d05e0ca32dc14e0ce162ee19931c
Born-S1nner <cookie.twenty01@gmail.com>
2021-01-09 15:22:42 -0500

    adding base.py to main file

--- base.py
+++ base.py
@@ -8,22 +8,22 @@ def system():
     results = ""
     errors = ""
     if request.method == "POST":
-        body = request.get_json()
+        body = request.form
         numberOne = None
         numberTwo = None
         var = None
         try:
-            numberOne = float(body.get('numone'))
+            numberOne = float(body['numone'])
             print(numberOne)
         except:
             errors += "<p>Number Slot one is not a number</p>"
         try:
-            numberTwo = float(body.get('numtwo'))
+            numberTwo = float(body['numtwo'])
             print(numberTwo)
         except:
             errors += "<p>Number Slot Two is not a number</p>"
         try:
-            var = body.get('var')
+            var = body['var']
             print(var)
         except:
             errors += "<p>Variable isn't present</p>"
@@ -33,11 +33,13 @@ def system():
             plusResult = plus(numberOne, numberTwo)
             print(plusResult)
             return render_template('index.html').format(result=plusResult, errors=errors)
+            
         if var == '-':
             print(var)
             minusResult = minus(numberOne, numberTwo)
             print(minusResult)
             return render_template('index.html').format(result=minusResult, errors=errors)
+            
     return render_template('index.html').format(result=results, errors=errors)    
 
 
